523e54e49cd5cc46e052be314dd73d2ba3f2abb4
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2023-10-20 22:30:51 +0900

    ラウンドカウント追加

--- bundles/bundle-1/graphics/index.js
+++ bundles/bundle-1/graphics/index.js
@@ -1,3 +1,7 @@
+let roundtype = 0
+let redround = 0
+let blueround = 0
+
 nodecg.Replicant('redplayer').on('change', (newVal) => {
     document.getElementById("redteam").innerText = newVal.name
 })
@@ -10,3 +14,46 @@ nodecg.Replicant('score').on('change', (newVal) => {
     document.getElementById("redscore").innerText = newVal.red
     document.getElementById("bluescore").innerText = newVal.blue
 })
+
+nodecg.Replicant('round').on('change', (newVal) => {
+    redround = newVal.red
+    blueround = newVal.blue
+    setRound()
+})
+
+nodecg.Replicant('roundtype').on('change', (newVal) => {
+    const redrounds = document.getElementById("redround")
+    while(redrounds.firstChild) redrounds.removeChild(redrounds.firstChild)
+    const bluerounds = document.getElementById("blueround")
+    while(bluerounds.firstChild) bluerounds.removeChild(bluerounds.firstChild)
+    roundtype = newVal.type
+
+    for (let i = 0; i < newVal.type; i++){
+        const redspan = document.createElement("span")
+        redspan.id = "redround." + i
+        redspan.className = "roundgray"
+        redrounds.appendChild(redspan)
+        const bluespan = document.createElement("span")
+        bluespan.id = "blueround." + i
+        bluespan.className = "roundgray"
+        bluerounds.appendChild(bluespan)
+    }
+
+    setRound()
+})
+
+function setRound(){
+    let count = roundtype
+    for(let i = 0; i < count; i++){
+        if (i < redround){
+            document.getElementById("redround." + (roundtype - i - 1)).className = "roundwhite"
+        }else {
+            document.getElementById("redround." + (roundtype - i - 1)).className = "roundgray"
+        }
+        if (i < blueround){
+            document.getElementById("blueround." + i).className = "roundwhite"
+        }else {
+            document.getElementById("blueround." + i).className = "roundgray"
+        }
+    }
+}
