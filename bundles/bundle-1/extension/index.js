b145210b5c0a43ff6922805bb37d19fcd4c72e3e
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2026-02-28 19:06:53 +0900

    ペコペコバトル用にカスタマイズ＆Challongeのバージョンアップに対応

--- bundles/bundle-1/extension/index.js
+++ bundles/bundle-1/extension/index.js
@@ -1,16 +1,150 @@
-module.exports = function(nodecg) {
-    const router = nodecg.Router();
+let api_key = ""
 
-    router.post('/scoreapi', (req, res) => {
-		const data = req.body;
-		const valuie = {
-			red : data.score_a,
-			blue : data.score_b
+function GenCommonHeader(){
+	var myHeaders = new Headers()
+	myHeaders.append("Accept", "application/json")
+	myHeaders.append("Authorization-Type", "v1")
+	myHeaders.append("Content-Type", "application/vnd.api+json")
+	myHeaders.append("Authorization", api_key)
+	return myHeaders
+}
+
+// チーム一覧をメモリ上に展開
+async function GetTeams(id){
+	const myHeaders = GenCommonHeader()
+
+	const requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow'
+	};
+
+	const datas = {}
+
+	await fetch("https://api.challonge.com/v2.1/tournaments/" + id + "/participants.json", requestOptions)
+		.then(response => response.json())
+  	.then(result => {
+			// 試合を順番に処理
+			result["data"].forEach((team) => {
+				datas[team["id"]] = team["attributes"]["name"]
+			})
+		})
+		.catch(error => console.log('error', error))
+
+	return datas
+}
+
+module.exports = async function(nodecg) {
+	const id = nodecg.bundleConfig.challonge_id
+	api_key = nodecg.bundleConfig.challonge_key
+	const router = nodecg.Router()
+	const teams = await GetTeams(id)
+
+	// 待機画面メッセージ初期化
+
+	// 試合一覧取得
+	router.get('/challonge/matches', async (req, res) => {
+		var myHeaders = GenCommonHeader()
+
+		var requestOptions = {
+			method: 'GET',
+			headers: myHeaders,
+			redirect: 'follow'
+		};
+
+		const res_data = { datas: {} }
+		let data_count = 0
+
+		await fetch("https://api.challonge.com/v2.1/tournaments/" + id + "/matches.json", requestOptions)
+		.then(response => response.json())
+  	.then(result => {
+			// 試合を順番に処理
+			result["data"].forEach((match) => {
+				// 得点を抽出
+				const score = []
+				match["attributes"]["points_by_participant"].forEach((score_data) => {
+					score.push({
+						id: score_data["participant_id"],
+						score: score_data["scores"]
+					})
+				})
+
+				// アプリで使いやすいように加工
+				res_data["datas"]["match_" + match["attributes"]["suggested_play_order"]] = {
+					id: match["id"],
+					state: match["attributes"]["state"],
+					round: match["attributes"]["round"],
+					winner: match["attributes"]["state"] == "complete" ? teams[match["attributes"]["winner_id"]] : "",
+					red: {
+						id: score[0]["id"],
+						team_name: score[0]["id"] ? teams[score[0]["id"]] : "",
+						score: score[0]["score"]
+					},
+					blue: {
+						id: score[1]["id"],
+						team_name: score[1]["id"] ? teams[score[1]["id"]] : "",
+						score: score[0]["score"]
+					}
+				}
+				data_count++
+			})
+		})
+		.catch(error => console.log('error', error))
+
+		res_data["match_count"] = data_count
+		res.status(200).json(res_data)
+	});
+
+	// 試合結果送信
+	router.post('/challonge/record', async (req, res) => {
+		const data = req["body"];
+		const red_rank = data["winner"] == data["match"]["red"]["team_name"] ? 1 : 2
+		const blue_rank = data["winner"] == data["match"]["blue"]["team_name"] ? 1 : 2
+		const req_body = {
+			data: {
+				type: "Match",
+				attributes: {
+					state: "complete",
+					match: [
+						{
+               participant_id: data["match"]["red"]["id"],
+               score_set: data["red_score"].toString(),
+               rank: red_rank,
+							 advancing: red_rank == 1
+            },
+						{
+               participant_id: data["match"]["blue"]["id"],
+               score_set: data["blue_score"].toString(),
+               rank: blue_rank,
+							 advancing: blue_rank == 1
+            }
+					],
+					tie: false
+				}
+			}
 		}
-		const Rep = nodecg.Replicant('score');
-		Rep.value = valuie;
-        res.send('OK!');
-    });
 
-    nodecg.mount('/api', router); // The route '/api/scoreapi` is now available
+		const requestOptions = {
+			method: 'PUT',
+			headers: GenCommonHeader(),
+			body: JSON.stringify(req_body),
+			redirect: 'follow'
+		};
+		await fetch("https://api.challonge.com/v2.1/tournaments/" + id + "/matches/" + data["match"]["id"] + ".json", requestOptions)
+		.then(response => {
+			if (response.ok){
+				res.status(200).json({
+					status: "success"
+				})
+			}
+			else {
+				res.status(500).json({
+					status: "failed",
+					response: response.text
+				})
+			}
+		})
+	})
+
+	nodecg.mount('/api', router);
 };
---
+++ memo.md
@@ -0,0 +1,59 @@
+# 使い方
+
+「WORKSPACE」で画面管理を行い、「GRAPHICS」からリンクをコピーして、OBSのブラウザソースとして取り込むことで動かします。
+GRAPHICSは試合画面用の「INDEX.HTML」と待機画面用の「WAITING.HTML」があります。
+
+## ScoreEdit
+
+スコアを編集します。「+/-/Reset」ボタンでスコアを編集し、「結果を送信する」ボタンでChallongeにスコアを送信します。
+後述の「PlayerSettings」から選手名を反映している場合は、結果送信機能は使用できません。
+
+## Match Select
+
+Challongeから試合を選んで、画面に選手名を反映します。「更新」ボタンで一覧を再取得できます。
+すでに結果が入っている試合は背景が緑、まだ結果が入っていない試合は背景が水色になっています。
+
+## PlayerSettings
+
+Challongeから選手名を反映できない際の予備機能。
+RedとBlueをそれぞれ選択し、「Update」ボタンを押下することで画面に反映される。
+
+## Tournament View
+
+Challongeのトーナメントを表示します。わざわざChallongeを開かなくても良いように置いてます。
+
+## WaitingEdit
+
+待機画面の右下のメッセージを更新できます。「更新」ボタンで反映されます。
+メッセージが長いと見切れるので、目安として全角文字だと23文字までにするほうがいいです。
+
+# 起動方法
+
+* nodejsをインストール
+  * 開発はv22.14.0で開発
+* nodecgをインストール
+  * npm install --global nodecg@latest
+* cfg配下に以下のファイルを作成（設定ファイルの内容は「設定」を参照）
+  * bundle-1.yml
+  * nodejs.yml
+* サーバー起動
+  * npx nodecg start
+
+# 設定
+
+## bundle-1.yml
+
+* challonge_key
+ChallongeのAPI v1 Keyを貼り付け
+* challonge_id
+対象のトーナメントのIDを記述
+* players
+Challongeからプレイヤーの反映ができない時の予備
+リスト形式でプレイヤー名を記述
+
+## nodejs.yml
+
+* host
+ホスト名
+* port
+ポート番号
